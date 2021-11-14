e87e5df56c8def47a6728d9836b42f7e53c285e7
DHARMATEJAGUGGILAM <guggilamdharma@msitprogram.net>
2021-11-14 19:13:48 +0530

    Convert DNA to RNA

--- hw6_protein.py
+++ hw6_protein.py
@@ -32,7 +32,16 @@ Parameters: str ; int
 Returns: list of strs
 '''
 def dnaToRna(dna, startIndex):
-    return
+    x=[]
+    y=[]
+    for i in range(startIndex, len(dna), 3):
+        x.append(dna[i:i+3])
+        if dna[i:i+3]=='TAG' or dna[i:i+3]=='TAA' or dna[i:i+3]=='TGA':
+            break
+    for string in x:
+        string=string.replace("T","U")
+        y.append(string)
+    return y
 
 
 '''
@@ -195,7 +204,8 @@ if __name__ == "__main__":
     # test.week1Tests()
     # print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
     # runWeek1()
-    test.testReadFile()
+    #test.testReadFile()
+    test.testDnaToRna()
 
     ## Uncomment these for Week 2 ##
     """
